85bbabe3560e44cfb1439f311b8f4cb38740a5e9
JokotoyeStephen <jokotoyestephen2023@gmail.com>
2025-10-10 10:58:18 +0100

    work in process

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+// import express from "express"
+// import path from 'path';
+// import dotenv from 'dotenv';
+
+
+
+// // Start the server
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// console.log("hello world");
+
+// const a = 5 + "5"
+// console.log(a);
+
+// const sum =(a,b)=>{
+//  return a + b;
+// //  return left = a - b
+// }
+// let add = sum(5,6)
+// console.log(add)
+
+// let superhero=()=>({
+//     const superhero = "Batman" 
+//     console.log(superhero)
+// })
+// superhero()
+
+import path from "path"
+// const path = require("path")
+
+// let superhero=()=>{
+//     const superhero = "Batman"
+//     const heelo = 'spiderman' 
+//     console.log(superhero)
+//     console.log(heelo)
+// } 
+
+// superhero("")
+
+console.log(path.join(__dirname, "stephen.js"));
+
+
